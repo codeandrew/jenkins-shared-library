e4d78e4620a1f48f1fb772fe82138822e10a748f
Jean Andrew Fuentes <jeanandrewfuentes@gmail.com>
2023-01-23 09:59:40 +0800

    sysInfo

--- vars/sysInfo.groovy
+++ vars/sysInfo.groovy
@@ -3,5 +3,14 @@ def call(){
   cat /etc/*-release
   lscpu  | grep -E 'Architecture:|CPU\(s\):'
   free -g
+
+  echo "[+] Checking HTTP CONNECTION"
+  response=$(curl -s -o /dev/null -w "%{http_code}" example.com)
+
+  if [ $response -eq 200 ]; then
+      echo "HTTP connection ... Succesful"
+  else
+      echo "[-] HTTP connection ... Failed"
+  fi
   """
 }

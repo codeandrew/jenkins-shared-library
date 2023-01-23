c9a114a3599a47d440c76058ef818d362d232d19
Jean Andrew Fuentes <jeanandrewfuentes@gmail.com>
2023-01-23 20:35:36 +0800

    Update setupEnvironment.groovy

--- vars/setupEnvironment.groovy
+++ vars/setupEnvironment.groovy
@@ -1,16 +1,13 @@
 def call(Map config = [:]) {
   sh '''
 
-  bash
-  printenv
-
-
-  if [[ -f microservice-templates ]]; then
+  if [ -f microservice-templates ]; then
     cd microservice-templates
   else
-    git clone https://github.com/codeandrew/microservice-templates.git
-    cd microservice-templates
+      git clone https://github.com/codeandrew/microservice-templates.git
+      cd microservice-templates
   fi
+
 
   mkdir -p $HOME/mongo/crud
   docker volume create --driver local --opt type=none --opt device=$HOME/mongo/crud --opt o=bind mongo_crud

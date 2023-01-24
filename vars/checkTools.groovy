88fbcab7a96f204f78558a6dfcb1730d7882d590
Jean Andrew Fuentes <jeanandrewfuentes@gmail.com>
2023-01-24 21:41:10 +0800

    improve format

--- vars/checkTools.groovy
+++ vars/checkTools.groovy
@@ -1,19 +1,23 @@
 def call(){
   sh '''#!/bin/bash
-  tools=("docker" "docker-compose" "helm" "ping" "wget" "curl" "traceroute" "dig" "netcat" "nc")
+    tools=("docker" "docker-compose" "helm" "ping" "wget" "curl" "traceroute" "dig" "netcat" "nc")
 
-  # Function to check if a tool is present
-  check_tool () {
-      if command -v $1 > /dev/null; then
-          echo "[+] $1 is present"
-      else
-          echo "[-] $1 is not present"
-      fi
-  }
-  
-  for tool in "${tools[@]}"; do
-      check_tool $tool
-  done
-  '''
+    # Function to check if a tool is present
+    check_tool () {
+        if command -v $1 > /dev/null; then
+            echo "[+] $1 is present"
+        else
+            echo "[-] $1 is not present"
+        fi
+    }
+
+    echo "=================================="
+    echo "[!] Checking Tools
+    echo "=================================="
+
+    for tool in "${tools[@]}"; do
+        check_tool $tool
+    done
+'''
 }
 

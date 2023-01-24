8e18016a1ed63cf8ad45d6b4c4c381b0a16a2182
Jean Andrew Fuentes <jeanandrewfuentes@gmail.com>
2023-01-24 21:45:47 +0800

    improve format

--- vars/checkTools.groovy
+++ vars/checkTools.groovy
@@ -20,4 +20,3 @@ def call(){
     done
 '''
 }
-
--- vars/helmInstall.groovy
+++ vars/helmInstall.groovy
@@ -1,10 +1,9 @@
-def call(){
-  sh '''#!/bin/bash
+def call() {
+  sh """#!/bin/bash
     curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
     chmod 700 get_helm.sh
     ./get_helm.sh
 
     helm version
-'''
+  """
 }
-

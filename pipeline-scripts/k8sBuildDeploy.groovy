bb60e7ef05cb59198029fe09f61eb68e5609e56e
Jean Andrew Fuentes <jeanandrewfuentes@gmail.com>
2023-01-25 22:32:57 +0800

    Working K8sBuildDeploy

--- pipeline-scripts/k8sBuildDeploy.groovy
+++ pipeline-scripts/k8sBuildDeploy.groovy
@@ -9,14 +9,6 @@ pipeline {
         stage("Test") {
             steps {
                 helloWorld("Jean Andrew", "DevOps Engineer")
-                sh ''' #!/bin/bash
-                docker ps
-                docker images
-                echo "[+] BUILD NUMBER:"
-                echo ${BUILD_NUMBER}
-                echo $app_version
-                '''
-
             }
         }
         
@@ -32,6 +24,7 @@ pipeline {
               setupHelm()
             }
         }
+
         stage('Build'){
             steps {
                 dockerBuild(
@@ -41,46 +34,26 @@ pipeline {
                 )
             }
         }
+
         stage('Package Helm'){ 
             steps {
-                sh '''#!/bin/bash
-                echo ${BUILD_NUMBER}
-                VERSION=$(date +'%y.%m.%d')-$BUILD_NUMBER
-                sed -i "s/APPVERSION/$VERSION/" chart/Chart.yaml
-                sed -i "s/APPVERSION/$VERSION/" chart/values-*.yaml
-
-                ENVIRONMENT=dev
-                helm template ./chart --values ./chart/values.yaml \
-                    --values ./chart/values-dev.yaml \
-                    --set app.tag=$VERSION \
-                    --set env.app_version=$VERSION \
-                    --set app.environment=$ENVIRONMENT \
-                    --output-dir ./helmtemplates
-                ls -altR ./helmtemplates
-
-                cat ./helmtemplates/fastapi/templates/*.yaml
-                '''
+                helmPackage(
+                    version: "${env.app_version}",
+                    environment: "test"
+                )
             }
         }
 
         stage('Deploy To Kubernetes'){
             steps {
-                sh '''#!/bin/bash
-                NAMESPACE=app
-                VERSION=$(date +'%y.%m.%d')-$BUILD_NUMBER
-                echo "======================================================"
-                echo "APPLICATION VERSION : $VERSION"
-                echo "======================================================"
-
-                kubectl apply -f helmtemplates/fastapi/templates -n $NAMESPACE
-                kubectl get pods -n app
-                kubectl get ingress -n app
+                k8sDeploy(
+                    namespace: "app",
+                    environment: "test",
+                    version: "${env.app_version}"
+                )
                 
-                '''
             }
         }
-
-
 
     }
 }

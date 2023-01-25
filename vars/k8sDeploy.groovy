fcf4f3c3c3c92f3aefd6dbd6f9b90211f945e4ea
Jean Andrew Fuentes <jeanandrewfuentes@gmail.com>
2023-01-25 22:28:19 +0800

    Create K8s Deploy

---
+++ vars/k8sDeploy.groovy
@@ -0,0 +1,15 @@
+def call(Map config = [:]) {
+  sh """bin/bash
+  NAMESPACE="${config.namespace}"
+  VERSION="${config.version}"
+  echo "======================================================"
+  echo "APPLICATION VERSION : \$VERSION"
+  echo "Environment: ${config.environment}"
+  echo "======================================================"
+
+  kubectl apply -f helmtemplates/fastapi/templates -n \$NAMESPACE
+  kubectl get pods -n \$NAMESPACE
+  kubectl get ingress -n \$NAMESPACE
+  """
+
+}

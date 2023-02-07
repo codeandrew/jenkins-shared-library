e81904138d6bd2d0b8d2a4880e5cc3530c302503
Jean Andrew Fuentes <jeanandrewfuentes@gmail.com>
2023-02-07 22:25:32 +0800

    trivy update db

--- vars/setupTrivy.groovy
+++ vars/setupTrivy.groovy
@@ -2,5 +2,6 @@ def call(Map config = [:]) {
   sh """
   curl -sfL https://raw.githubusercontent.com/aquasecurity/trivy/main/contrib/install.sh | sh -s -- -b /usr/local/bin v0.36.1
   trivy -v
+  trivy image --download-db-only
   """
 }

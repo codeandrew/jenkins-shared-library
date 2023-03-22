42516404f10c621e6041a7b2c1ef7effaf57c3f9
Jean Andrew Fuentes <jeanandrewfuentes@gmail.com>
2023-03-22 22:58:40 +0800

    Update typo

--- vars/trivyScannerRepo.groovy
+++ vars/trivyScannerRepo.groovy
@@ -1,10 +1,11 @@
 def call(Map config = [:]) {
     sh """ #!/bin/bash
     output_path=${config.outputPath}
-    report_name=$(basename $(pwd))
+    mkdir -p \$output_path
+    repo_name=$(basename $(pwd))
     version=\$(date +'%y.%m.%d')
-    report_name=\$BUILD_ID-/$report_name-\$version.txt
-    result=\$(trivy fs . -o $report_name )
+    report_name=\$BUILD_ID-/$repo_name-\$version.txt
+    result=\$(trivy fs . -o $output_path/\$report_name )
 
     echo \$result
     ls -altr \$output_path

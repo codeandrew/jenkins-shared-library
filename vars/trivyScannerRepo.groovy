69e92817da678cdf7c72a3ca3b58e2584eb8e8e6
Jean Andrew Fuentes <jeanandrewfuentes@gmail.com>
2023-03-31 16:41:51 +0800

    Change Variable OUTPUT_DIR

--- vars/trivyScannerRepo.groovy
+++ vars/trivyScannerRepo.groovy
@@ -1,16 +1,16 @@
 def call(Map config = [:]) {
     sh """ #!/bin/bash
     set -x
-    export output_path=${config.outputPath}
+    export OUTPUT_DIR=${config.outputPath}
     
     repo_name=\$(basename \$(pwd))
     version=\$(date +'%y.%m.%d')
-    report_name=\$BUILD_ID-\$repo_name-\$version.txt
+    REPORT_NAME=\$BUILD_ID-\$repo_name-\$version.txt
     
-    mkdir -p \$output_path
-    result=\$(trivy fs . -o $output_path/\$report_name )
+    mkdir -p \$OUTPUT_DIR
+    result=\$(trivy fs . -o $OUTPUT_DIR/\$REPORT_NAME )
 
     echo \$result
-    ls -altr \$output_path
+    ls -altr \$OUTPUT_DIR
     """
 }

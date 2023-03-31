ca4f393838bb92908c1d80155b581405d931234c
Jean Andrew Fuentes <jeanandrewfuentes@gmail.com>
2023-03-31 14:44:21 +0800

    test trivyScannerRepo illegal string

--- vars/trivyScannerRepo.groovy
+++ vars/trivyScannerRepo.groovy
@@ -1,11 +1,12 @@
 def call(Map config = [:]) {
     sh """ #!/bin/bash
-    set -x
     output_path=${config.outputPath}
-    mkdir -p \$output_path
+    
     repo_name=$(basename $(pwd))
     version=\$(date +'%y.%m.%d')
     report_name=\$BUILD_ID-/$repo_name-\$version.txt
+    
+    mkdir -p \$output_path
     result=\$(trivy fs . -o $output_path/\$report_name )
 
     echo \$result

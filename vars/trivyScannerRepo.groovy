55bdc68044c3e37690e7e975259d0eddba9dae54
Jean Andrew Fuentes <jeanandrewfuentes@gmail.com>
2023-03-31 17:08:44 +0800

    Debuggimg 4

--- vars/trivyScannerRepo.groovy
+++ vars/trivyScannerRepo.groovy
@@ -2,18 +2,14 @@ def call(Map config = [:]) {
     sh """ #!/bin/bash
     set -x
     output_path=${config.outputPath}
-    echo \$output_path
-    
     repo_name=\$(basename \$(pwd))
-    
+    version=\$(date +'%y.%m.%d')
+    REPORT_NAME=\$BUILD_ID-\$repo_name-\$version.txt
+
+    pwd; ls -latr
+    printenv | sort
     """
 }
-
-    
-    
-    // version=\$(date +'%y.%m.%d')
-    // REPORT_NAME=\$BUILD_ID-\$repo_name-\$version.txt
-    
     // mkdir -p \$OUTPUT_DIR
     // result=\$(trivy fs . -o $OUTPUT_DIR/\$REPORT_NAME )
 

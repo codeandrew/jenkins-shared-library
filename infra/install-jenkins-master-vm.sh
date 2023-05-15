acd5cb733b606746838a8a63cb02d0615543a5b5
Jean Andrew Fuentes <jeanandrewfuentes@gmail.com>
2023-05-15 17:41:24 +0800

    Create install-jenkins-master-vm.sh

---
+++ infra/install-jenkins-master-vm.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -x
+curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee \
+  /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
+  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
+sudo apt-get update
+sudo apt-get install openjdk-11-jre jenkins
+
+java -version
+sudo systemctl enable jenkins
+sudo systemctl start jenkins
+
+cat /var/jenkins_home/secrets/initialAdminPassword

a76267580d8b334c370af9d11d4be7d9ad176f3d
Jean Andrew Fuentes <jeanandrewfuentes@gmail.com>
2023-07-10 21:19:50 +0800

    Typo

--- vars/trivyContainerScanner.groovy
+++ vars/trivyContainerScanner.groovy
@@ -12,7 +12,7 @@ def call(Map config = [:]) {
 
     RESULT_PATH=\$output_path/\$report_name
     docker pull \$target_image
-    trivy image  --no-progress -f json -o \$RESULT_PATH.json $target_image    
+    trivy image  --no-progress -f json -o \$RESULT_PATH.json \$target_image    
     result=\$(trivy image  -o \$RESULT_PATH.txt \$target_image )
 
     echo \$result
